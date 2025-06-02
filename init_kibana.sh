0e4506f4bb0d1ea1b7d76f5a1e87844ff5d75aad
Chanagun Viriyasathapornpong <chanagun.vir@gmail.com>
2025-06-02 18:38:06 +0700

    fix: change index pattern with rm duplicate script

--- init_kibana.sh
+++ init_kibana.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-echo "⏳ Waiting for Kibana..."
-until curl -s http://kibana:5601/api/status | grep -q '\"state\":\"green\"'; do
+echo "⏳ Waiting for Kibana to become ready..."
+until curl -s http://kibana:5601/api/status | grep -q '"state":"green"'; do
+  echo "⌛ Kibana not ready yet..."
   sleep 5
 done
 
-echo "🚀 Importing saved objects..."
-curl -X POST http://kibana:5601/api/saved_objects/_import?overwrite=true \
-     -H "kbn-xsrf: true" \
-     -F file=@/tmp/kibana.ndjson
+echo "🚀 Creating index pattern from JSON file..."
+curl -X POST http://kibana:5601/api/saved_objects/index-pattern \
+  -H 'Content-Type: application/json' \
+  -H 'kbn-xsrf: true' \
+  -d @/kibana/create_index.json  # adjust this path if needed
 
-echo "✅ Import completed!"
+echo "✅ Done creating index pattern!"
